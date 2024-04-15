764ef49a9c5ee4c1719cfc4581c012b7ff0b495e
HuzaifaAnsari0 <huzaifa.a@somaiya.edu>
2024-04-15 11:19:06 +0530

    login access

--- programs.php
+++ programs.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit();
+}
+
 $host = "localhost";
 $dbname = "Eduford";
 $user = "postgres";
